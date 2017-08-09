77175e903b961ba860d90af5ba8f5ce1a2daed41
robertpiira <robert@piira.se>
2017-08-09 17:07:53 +0200

    added tests

--- tests/tests.js
+++ tests/tests.js
@@ -1,5 +1,4 @@
 const test = require ('tape')
-const async = require('async')
 const swish = require('../dist/swish')
 
 test('Successful flow (init -> add -> get)', t => {
@@ -17,53 +16,34 @@ test('Successful flow (init -> add -> get)', t => {
     }
   }
 
-
   swish.init(configData).then(result => {
-    // 1 - Find private key
     t.notEqual(-1, result.cert.key.indexOf('BEGIN RSA PRIVATE KEY'), 'Found private key')
-    // 2 - Find certificate
     t.notEqual(-1, result.cert.cert.indexOf('BEGIN CERTIFICATE'), 'Found certificate')
-    // 3 - Find root certificate
     t.notEqual(-1, result.cert.ca.indexOf('BEGIN CERTIFICATE'), 'Found root certificate')
-    // 4 - Find passphrase
     t.equal(configData.cert.passphrase, result.cert.passphrase, 'Found passphrase')
-    // 5 - Find payeeAlias
     t.equal(configData.data.payeeAlias, result.data.payeeAlias, 'Found payee alias')
-    // 6 - Find payeeAlias
     t.equal(configData.data.callbackUrl, result.data.callbackUrl, 'Found callback url')
 
     t.end()
-  }).catch(err => {
-    t.equal(true, true, 'should be true')
+  })
+
+  swish.add({
+    payeePaymentReference: 'snus123',
+    payerAlias: '0706123456',
+    amount: '100',
+    message: 'Prima snus'
+  })
+  .then(id => {
+    t.equal(id.length, 32, 'Response: 200 / valid ID (#{id})')
+    return swish.get(id)
+  })
+  .then(result => {
+    t.equal(id, result.id, 'Response: 200 / found (#{id})')
+    t.end()
+  })
+  .catch(err => {
+    t.fail(JSON.stringify(error))
     t.end()
   })
 
-
 })
-
-
-
-
-
-  // # 2 - ADD
-  // .then ->
-  //
-  //   paymentData =
-  //     payeePaymentReference: '1234'
-  //     payerAlias: '0709123456'
-  //     amount: '100'
-  //     message: 'test'
-  //
-  //   swish.add paymentData
-  //
-  // .then (@id) ->
-  //   # 7 - Get id
-  //   t.equal id.length, 32, 'Response: 200 / valid ID (#{id})'
-  //   swish.get id
-  // .then (result) ->
-  //   console.log result
-  //   t.equal id, result.id, 'Response: 200 / found (#{id})'
-  //   t.end()
-  // .catch (err) ->
-  //   t.fail JSON.stringify error
-  //   t.end()
